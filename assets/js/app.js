2d24391c03b3ad5644af12ef86b5b02f91b240c4
vincentsig <vincent.signoret.mail@gmail.com>
2020-02-17 10:59:40 -0500

    fixing error with webpack Encore

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,14 +6,14 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+import '../css/app.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
 
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+console.log('Hello Webpack Encore!!!!!!!n Edit me in assets/js/app.js');
 
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/js/all.js');
